fcd9cfb25bb04eb857fc75ac8c98cb58a6a4e969
nixpig <143995476+nixpig@users.noreply.github.com>
2023-09-27 04:17:23 +0100

    chore: tidy up mcfly script

--- .local/bin/fix_legacy_tiocsti.sh
+++ .local/bin/fix_legacy_tiocsti.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
+
 if [[ $(sysctl -n dev.tty.legacy_tiocsti) == 0 ]]; then
-	sysctl -w dev.tty.legacy_tiocsti=1 &1>/dev/null
-	exit 0
+	read -n 1 -p "Enable legacy_tiocsti? [y/N]: " answer
+	case ${answer:0:1} in
+		y|Y )
+			sudo sysctl -w dev.tty.legacy_tiocsti=1 &1>/dev/null
+		;;
+		* )
+			echo "\nExiting"
+			exit 0
+		;;
+	esac
 fi
